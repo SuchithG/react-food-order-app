74c825db6e20f5dcecd7604a243c315257904aae
SuchithG <suchithgowda1995@gmail.com>
2023-03-16 19:05:54 +0530

    feat: :zap: Adding a modal via portal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react";
 
+import Cart from "./components/Cart/Cart.js";
 import Header from "./components/Layout/Header.js";
 import Meals from "./components/Meals/Meals.js";
 
 function App() {
   return (
     <Fragment>
+      <Cart/>
       <Header />
       <main>
         <Meals />
