939d37e12f9a81a016364c389efd3162071b7050
SuchithG <suchithgowda1995@gmail.com>
2023-03-20 23:52:52 +0530

    feat: :construction: Managing cart and modal state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 import Cart from "./components/Cart/Cart.js";
 import Header from "./components/Layout/Header.js";
 import Meals from "./components/Meals/Meals.js";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
   return (
     <Fragment>
       <Cart/>
