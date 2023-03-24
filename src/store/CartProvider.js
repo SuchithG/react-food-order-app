92e13a9b126eac68e4926568dd18fbc67da52b2d
SuchithG <suchithgowda1995@gmail.com>
2023-03-24 09:33:02 +0530

    feat: :art: Making items removable

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -6,19 +6,19 @@ const defaultCartState = {
   items: [],
   totalAmount: 0,
 };
- 
+
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
     const updateTotalAmount =
       state.totalAmount + action.item.price * action.item.price;
 
-    const existingCartItemsIndex = state.items.findIndex(
+    const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.item.id
     );
 
-    const existingCartItem = state.items[existingCartItemsIndex];
+    const existingCartItem = state.items[existingCartItemIndex];
     let updatedItems;
-    
+
     if (existingCartItem) {
       const updatedItem = {
         ...existingCartItem,
@@ -35,6 +35,28 @@ const cartReducer = (state, action) => {
       totalAmount: updateTotalAmount,
     };
   }
+
+  if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
   return defaultCartState;
 };
 
